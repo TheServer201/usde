19f69c76870b5890d0dc47c216a5c800833a809c
TheServer201 <TheServer201@users.noreply.github.com>
2016-07-13 23:35:16 +0200

    Update diasm.c

--- diasm.c
+++ diasm.c
@@ -6,42 +6,62 @@ typedef struct {
 } Insn;
 
 uint8_t DiAsm(const uint8_t *Code, Insn *Result){
-	uint8_t Size, Byte, Low0, Low1, High, Mod;
-	for(Size = 0x10; Size; Size -= 0x01){
+	uint8_t Size, Byte, Low0, Low1, High, Imm;
+	for(Size = 0x00; Size < 0x10; Size += 0x01){
 		Byte = Code[Size];
 		Low0 = Byte & 0x07;
-		if(Result->Mod){
+		if(Result->Flg & 0x08){
+			Imm = Result->Flg >> 0x04;
+			if(Imm){
+				Result->Imm |= Byte << (0x08 * (0x04 - Imm));
+				Result->Flg -= 0x10;
+			} else
+				Result->Flg &= ~0x08;
+		}
+		if(Result->Flg & 0x04){
+			Result->Bas = Low0;
+			Result->Sca = Byte >> 0x06;
+			Result->Ind = (Byte >> 0x03) & 0x07;
+			Result->Flg &= ~0x04;
+			if(Result->Bas == 0x05)
+				Result->Flg |= 0x48;
+		}
+		if(Result->Flg & 0x02){
 			Result->Rm  = Low0;
 			Result->Mod = Byte >> 0x06;
 			Result->Reg = (Byte >> 0x03) & 0x07;
 			if(Result->Mod != 0x03){
 				if(Result->Pre & 0x08){
-					if(Result->Rm == 0x06)
-						Result->Imm = 0x02;
+					if((Result->Mod == 0x02) || 
+					   (Result->Rm  == 0x06))
+						Result->Flg |= 0x28;
 				} else {
-					if(Result->Rm == 0x05)
-						Result->Imm = 0x04;
+					if((Result->Mod == 0x02) ||
+					   (Result->Rm  == 0x05))
+						Result->Flg |= 0x48;
 					if(Result->Rm == 0x04)
-						Result->Sca = 0x01;
+						Result->Flg |= 0x04;
 				}
 				if(Result->Mod == 0x01)
-					Result->Imm = 0x01;
+					Result->Flg |= 0x18;
 			}
-		} else if(Result->Sca){
-			Result->Bas = Low0;
-			Result->Sca = Byte >> 0x06;
-			Result->Ind = (Byte >> 0x03) & 0x07;
-		} else if(Result->Imm)
-			Result->Imm = ((uint32_t*)Code)[Size] & (-0x01 >> 0x08 * (0x04 - Result->Imm));
-		else {
+			Result->Flg &= ~0x02;
+		}
+		if(Result->Flg < 0x02){
+			if(Size > 0x01)
+				goto Done;
+			Result->Opc = Byte;
 			Low1 = Byte & 0x0F;
 			High = Byte & 0xF0;
 			if(Result->Flg & 0x01){
-				if((High == 0x40) || (Byte == 0xAF) ||
-				   (Byte == 0xB6) || (Byte == 0xB7)){
-					Result->Mod = 0x01;
-				}
-			} else
+				if((High == 0x40) ||
+				   (Byte == 0xAF) || 
+				   (Byte == 0xB6) ||
+				   (Byte == 0xB7))
+					Result->Flg |= 0x02;
+				else
+					goto Done;
+			} else {
 				switch(Byte){
 					case 0xF2:
 						Result->Pre |= 0x01;
@@ -54,34 +74,32 @@ uint8_t DiAsm(const uint8_t *Code, Insn *Result){
 					case 0x0F:
 						Result->Flg = 0x01;
 					case 0xC3: case 0xC9:
-						Result->Flg = 0x02;
+						goto Done;
 					case 0xFE: case 0xFF:
-						Result->Mod = 0x01;
+						Result->Flg |= 0x02;
 					case 0x6A: case 0xEB:
-						Result->Imm = 0x01;
+						Result->Flg |= 0x18;
 					case 0xC2:
-						Result->Imm = 0x02;
+						Result->Flg |= 0x28;
 					case 0x68: case 0xE8:
 					case 0xE9:
-						Result->Imm = 0x04;
+						Result->Flg |= 0x48;
 					case 0x6B: case 0xC0:
 					case 0xC1: case 0xC6:
-						Result->Mod = 0x01;
-						Result->Imm = 0x01;
+						Result->Flg |= 0x1A;
 					case 0x69: case 0xC7:
-						Result->Mod = 0x01;
-						Result->Imm = 0x04;
+						Result->Flg |= 0x4A;
 					default:
 						switch(High){
 							case 0x70:
-								Result->Imm = 0x01;
+								Result->Flg |= 0x18;
 							case 0x80:
-								Result->Mod = 0x01;
+								Result->Flg |= 0x02;
 								if(Low1 < 0x04){
 									if(Low1 != 0x02)
-										Result->Imm = 0x01;
+										Result->Flg |= 0x18;
 									else
-										Result->Imm = 0x04;
+										Result->Flg |= 0x48;
 								}
 							case 0x90:
 								if(Low1 < 0x08)
@@ -89,9 +107,9 @@ uint8_t DiAsm(const uint8_t *Code, Insn *Result){
 							case 0xA0:
 								if(Low1 < 0x04){
 									if(Result->Pre & 0x08)
-										Result->Imm = 0x02;
+										Result->Flg |= 0x28;
 									else
-										Result->Imm = 0x04;
+										Result->Flg |= 0x48;
 								} else if(Low1 < 0x08)
 									Result->Reg = 0x20;
 								else {
@@ -101,36 +119,37 @@ uint8_t DiAsm(const uint8_t *Code, Insn *Result){
 										Result->Reg |= 0x10;
 									else
 										if(Low1 & 0x01)
-											Result->Imm = 0x04;
+											Result->Flg |= 0x48;
 										else
-											Result->Imm = 0x01;
+											Result->Flg |= 0x18;
 								}
 							case 0xB0:
 								Result->Reg = Low0;
 								if(Low1 < 0x08)
-									Result->Imm = 0x01;
+									Result->Flg |= 0x18;
 								else
-									Result->Imm = 0x04;
+									Result->Flg |= 0x48;
 							case 0xD0:
 								if(Low1 < 0x04)
-									Result->Mod = 0x01;
+									Result->Flg |= 0x02;
 							default:
 								if(High < 0x40){
 									if(Low1 < 0x06)
-										Result->Mod = 0x01;
+										Result->Flg |= 0x02;
 									else if(Low1 < 0x08){
 										Result->Reg = 0x08;
 										if(Low1 & 0x01)
-											Result->Imm = 0x04;
+											Result->Flg |= 0x48;
 										else
-											Result->Imm = 0x01;
+											Result->Flg |= 0x18;
 									}
 								} else if(High < 0x60)
 									Result->Reg = Low0;
 						}
 				}
-			Result->Opc = Byte;
+			}
 		}
 	}
+Done:
 	return Size;
 }
